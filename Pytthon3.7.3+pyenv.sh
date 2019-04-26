0b9ce5fc48671ed6c17141ca835de9d1975bbbd5
Migrant-worker <31792856+Migrant-worker@users.noreply.github.com>
2019-04-26 11:39:21 +0800

    Create Pytthon3.7.3+pyenv.sh

---
+++ Pytthon3.7.3+pyenv.sh
@@ -0,0 +1,26 @@
+# 准备工作
+
+
+# 安装git和必须的库
+yum -y install git
+yum -y install gcc make patch gdbm-devel openssl-devel sqlite-devel readline-devel zlib-devel bzip2-devel libffi-devel
+
+# 安装pyenv
+curl https://pyenv.run | bash
+
+# 更新pyenv
+pyenv update
+
+# 添加环境变量
+echo 'export PATH="~/.pyenv/bin:$PATH"' >> .bashrc
+echo 'eval "$(pyenv init -)"' >> .bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> .bashrc 
+
+# 重新加载.bashrc文件
+source .bashrc
+
+# 查看python版本
+python -V
+
+# 安装python
+pyenv install 3.7.3
